08e3acd2c027c5ca6f5af78ea8f31733afb73b9e
adrianscorus14 <adrianscorus14@gmail.com>
2020-12-01 14:07:26 +0200

    spring configuration added

---
+++ employe-management-spring/src/main/java/com/example/employemanagementspring/EmployeManagementSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.employemanagementspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeManagementSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeManagementSpringApplication.class, args);
+    }
+
+}
